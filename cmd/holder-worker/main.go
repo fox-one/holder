358daf4690deb582605d3792fca722bbd3fe685f
yiplee <guoyinl@gmail.com>
2021-09-27 18:56:54 +0800

    set cashier.capacity default to 10

--- cmd/holder-worker/main.go
+++ cmd/holder-worker/main.go
@@ -47,7 +47,7 @@ func init() {
 
 	// worker.cashier.Config
 	flag.IntVar(&_flag.cashier.batch, "cashier.batch", 100, "custom batch for worker cashier")
-	flag.Int64Var(&_flag.cashier.capacity, "cashier.capacity", 1, "custom capacity for worker cashier")
+	flag.Int64Var(&_flag.cashier.capacity, "cashier.capacity", 10, "custom capacity for worker cashier")
 
 	// worker.datadog.Config
 	flag.DurationVar(&_flag.datadog.interval, "datadog.interval", 5*time.Minute, "custom datadog trigger interval")
