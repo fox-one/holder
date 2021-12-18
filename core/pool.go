be09fb0d7928ec753b6ff3a96e121eac8f83449c
yiplee <guoyinl@gmail.com>
2021-12-18 14:58:46 +0800

    fix pool reform

--- core/pool.go
+++ core/pool.go
@@ -42,11 +42,7 @@ func (pool *Pool) Reform(vault *Vault) {
 		return
 	}
 
-	if vault.CreatedAt.After(pool.PardonedAt) {
-		return
-	}
-
-	if dur := pool.PardonedAt.Sub(vault.CreatedAt).Milliseconds() / 1000; dur < vault.Duration {
+	if dur := pool.PardonedAt.Sub(vault.CreatedAt).Milliseconds() / 1000; dur > 0 && dur < vault.Duration {
 		vault.Duration = dur
 		vault.MinDuration = dur
 	}
